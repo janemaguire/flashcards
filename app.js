924dcebc0e314861cf8113adb1c7589246709b61
Jane Maguire <jane.maguire@imagination.com>
2017-11-14 13:10:03 +0000

    cards route and template in pug

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const express = require('express');
 // This is convention
 const app = express();
 
+// Tells express which template engine to use, will ook in /views by default
+app.set('view engine', 'pug');
+
 // Home route will listen for GET requests, two parameters, path and callback function
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1>');
+    res.render('index');
 });
 
-// Hello route works same way as home
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello Developer</h1>')
-})
+// Cards route renders card template
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: 'Question 1', hint: 'Here is clue 1'});
+});
 
 // Port to serve app on, can also take a callback as a parameter
 app.listen(3000, () => {
