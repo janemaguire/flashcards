69f447ca7b41a84e9887376990ea7aa84b5355b4
Jane Maguire <jane.maguire@imagination.com>
2017-11-14 12:08:32 +0000

    hello route

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+// Require express module
 const express = require('express');
 
+// This is convention
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello World');
+// Home route will listen for GET requests, two parameters, path and callback function
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>');
 });
 
-app.listen(3000);
+// Hello route works same way as home
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Developer</h1>')
+})
+
+// Port to serve app on, can also take a callback as a parameter
+app.listen(3000, () => {
+    console.log('Running on port 3000');
+});
