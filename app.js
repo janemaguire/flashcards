2f4b3cda86207fe43975b351c6f0d3b7ffb01a7f
Jane Maguire <jane.maguire@imagination.com>
2017-11-14 16:16:58 +0000

    hello route post request

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// Require express module
+// Require modules
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // This is convention
 const app = express();
+
+// Use body bodyParser and turn off extended option
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Tells express which template engine to use, will ook in /views by default
 app.set('view engine', 'pug');
@@ -10,6 +14,17 @@ app.set('view engine', 'pug');
 // Home route will listen for GET requests, two parameters, path and callback function
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+// Hello route get request
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+// Hello route post request
+app.post('/hello', (req, res) => {
+    console.log(req.body);
+    res.render('hello');
 });
 
 // Cards route renders card template
